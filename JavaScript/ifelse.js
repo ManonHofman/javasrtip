1eec09c6c000a7578719d917b8b2cca8c9cac1fa
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-23 14:30:41 +0200

    Add files via upload

---
+++ JavaScript/ifelse.js
@@ -0,0 +1,33 @@
+
+const age = 19;
+const isFemale = true;
+const name = "Sarah";
+const driverStatus = "bob";
+
+if (age >= 18 && age <=25) {
+  console.log("Je krijgt 50% korting!");
+}
+
+if (isFemale) {
+  console.log("Je ticket zegt dat je een vrouw bent!");
+} else {
+  console.log(
+    "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+  );
+}
+
+if (name = "Sarah"|| name = "Abraham") {
+  console.log("Jij krijgt een gratis biertje!");
+} 
+
+const totalAmount = 56;
+
+if (totalAmount =>25 && totalAmount < 50){
+    console.log("Jij krijgt een gratis portie bitterballen")
+}
+if (totalAmount =>50 && totalAmount < 100){
+    console.log("Jij krijgt een gratis portie nacho's")
+}
+
+if (totalAmount =>100){
+    console.log("Jij krijgt een gratis flesje champagne!")
